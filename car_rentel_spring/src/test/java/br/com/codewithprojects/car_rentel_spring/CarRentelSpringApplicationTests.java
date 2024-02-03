d3551ce98855a30cafc7a48c07d1a54498d3709c
Lucas Lima <lucaserolima@gmail.com>
2024-02-02 20:57:40 -0400

    First commit

---
+++ car_rentel_spring/src/test/java/br/com/codewithprojects/car_rentel_spring/CarRentelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codewithprojects.car_rentel_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
