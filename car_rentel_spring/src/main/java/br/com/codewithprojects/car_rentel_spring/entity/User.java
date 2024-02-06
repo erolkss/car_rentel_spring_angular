953998a6bc1d75e9a24ee5bd5652ffae419496e3
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:42:48 -0400

    Adding anotation the properties Id in the Class User - SpringBoot

--- car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/entity/User.java
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/entity/User.java
@@ -1,14 +1,15 @@
 package br.com.codewithprojects.car_rentel_spring.entity;
 
 import br.com.codewithprojects.car_rentel_spring.enums.UserRole;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
