9357dcd80a2d32f0426a24fc7adbd75f62465535
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:13:08 -0400

    Adding properties to the User class

--- car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/entity/User.java
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/entity/User.java
@@ -1,5 +1,6 @@
 package br.com.codewithprojects.car_rentel_spring.entity;
 
+import br.com.codewithprojects.car_rentel_spring.enums.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -8,4 +9,9 @@ import lombok.Data;
 @Entity
 @Table(name = "users")
 public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private UserRole userRole;
 }
