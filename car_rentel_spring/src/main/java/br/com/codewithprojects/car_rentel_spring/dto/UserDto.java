578dae6c7eaf1bd27b1c98592d339987e293303a
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:25:37 -0400

    UserDto import packages

--- car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/dto/UserDto.java
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/dto/UserDto.java
@@ -1,6 +1,9 @@
 package br.com.codewithprojects.car_rentel_spring.dto;
 
 import br.com.codewithprojects.car_rentel_spring.enums.UserRole;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
