a4e8cea32319ed6cee728b39e286261721588b5d
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:35:03 -0400

    Implementing the 'CreateCustomer' method in 'AuthServiceImpl' - SpringBoot

--- car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/services/auth/AuthServiceImpl.java
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/services/auth/AuthServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.codewithprojects.car_rentel_spring.services.auth;
 
+import br.com.codewithprojects.car_rentel_spring.dto.SignupRequest;
+import br.com.codewithprojects.car_rentel_spring.dto.UserDto;
+import br.com.codewithprojects.car_rentel_spring.entity.User;
+import br.com.codewithprojects.car_rentel_spring.enums.UserRole;
 import br.com.codewithprojects.car_rentel_spring.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService{
     private final UserRepository userRepository;
+
+
+    @Override
+    public UserDto createCustomer(SignupRequest signupRequest) {
+
+        User user = new User();
+        user.setName(signupRequest.getName());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(signupRequest.getPassword());
+        user.setUserRole(UserRole.CUSTOMER);
+        User createUser = userRepository.save(user);
+        UserDto userDto = new UserDto();
+        userDto.setId(createUser.getId());
+        return userDto;
+    }
 }
