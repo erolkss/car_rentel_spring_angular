d12de1608ccc627c7b987b8d78249295ec1e23f0
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:30:10 -0400

    Created method 'createCustomer' in AuthService - SpringBoot

---
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/services/auth/AuthService.java
@@ -0,0 +1,10 @@
+package br.com.codewithprojects.car_rentel_spring.services.auth;
+
+import br.com.codewithprojects.car_rentel_spring.dto.SignupRequest;
+import br.com.codewithprojects.car_rentel_spring.dto.UserDto;
+
+public interface AuthService {
+
+    UserDto createCustomer(SignupRequest signupRequest);
+
+}
