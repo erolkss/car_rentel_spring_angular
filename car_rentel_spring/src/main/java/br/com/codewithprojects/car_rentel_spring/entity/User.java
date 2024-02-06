8bf5f5305c6788a18435622049844bd387eb4066
Lucas Lima <lucaserolima@gmail.com>
2024-02-05 21:07:54 -0400

    Created the User class and entity package

---
+++ car_rentel_spring/src/main/java/br/com/codewithprojects/car_rentel_spring/entity/User.java
@@ -0,0 +1,11 @@
+package br.com.codewithprojects.car_rentel_spring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+}
