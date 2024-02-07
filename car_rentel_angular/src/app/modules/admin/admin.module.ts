9a293bed79af752ede6d85f42f963d67c7aea53c
Lucas Lima <lucaserolima@gmail.com>
2024-02-07 15:51:40 -0400

    (New Project) Update - App module and component and admin module - Angular

--- car_rentel_angular/src/app/modules/admin/admin.module.ts
+++ car_rentel_angular/src/app/modules/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './components/login/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminDashboardComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
