9b1875bd4062b2247ef3861fb4f66f3c7fdfc67a
Lucas Lima <lucaserolima@gmail.com>
2024-02-06 21:21:26 -0400

    (New Project) Creating the module for Customer - Angular

---
+++ car_rentel_angular/src/app/modules/custumer/custumer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustumerRoutingModule } from './custumer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustumerRoutingModule
+  ]
+})
+export class CustumerModule { }
