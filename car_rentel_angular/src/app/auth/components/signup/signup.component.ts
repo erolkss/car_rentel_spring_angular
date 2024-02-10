0168ac11347526171230c6b3a353dc14612e7beb
Lucas Lima <lucaserolima@gmail.com>
2024-02-09 22:04:21 -0400

    Adding popup message when registering a New user - Angular

--- car_rentel_angular/src/app/auth/components/signup/signup.component.ts
+++ car_rentel_angular/src/app/auth/components/signup/signup.component.ts
@@ -6,6 +6,8 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -16,7 +18,12 @@ export class SignupComponent {
   isSpinning: boolean = false;
   signupForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -30,16 +37,22 @@ export class SignupComponent {
   confirmationValidate = (control: FormControl): { [s: string]: boolean } => {
     if (!control.value) {
       return { required: true };
-    }else if(control.value !== this.signupForm.controls['password'].value){
-      return {confirm: true, error: true};
+    } else if (control.value !== this.signupForm.controls['password'].value) {
+      return { confirm: true, error: true };
     }
     return {};
   };
 
   register() {
     console.log(this.signupForm.value);
-    this.authService.register(this.signupForm.value).subscribe((res) =>{
+    this.authService.register(this.signupForm.value).subscribe((res) => {
       console.log(res);
-    })
+      if (res.id != null) {
+        this.message.success('Signup Successful', { nzDuration: 5000 });
+        this.router.navigateByUrl("/login");
+      } else {
+        this.message.error('Something went Wrong', { nzDuration: 5000 });
+      }
+    });
   }
 }
