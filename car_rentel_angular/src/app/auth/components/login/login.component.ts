03d1bcae29b97e24bb3db910bd4ecf8b8499121c
Lucas Lima <lucaserolima@gmail.com>
2024-02-10 20:48:48 -0400

    Login form created - Angular

--- car_rentel_angular/src/app/auth/components/login/login.component.ts
+++ car_rentel_angular/src/app/auth/components/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  isSpinning: boolean = false;
 
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+    this.loginForm = this.fb.group({
+      email:[null, [Validators.email, Validators.required]],
+      password:[null, [Validators.required]]
+    })
+  }
+
+  login(){
+    console.log(this.loginForm.value);
+  }
 }
